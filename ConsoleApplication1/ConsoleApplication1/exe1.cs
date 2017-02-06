19a11beaf8ed27a13ab0e457764b3fe8c030d533
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-02-06 13:20:16 +0800

    exercise

---
+++ ConsoleApplication1/ConsoleApplication1/exe1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class exe1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Htein Lin Aung");
+            Console.WriteLine("hteinlinaung1423@gmail.com");
+        }
+    }
+}
