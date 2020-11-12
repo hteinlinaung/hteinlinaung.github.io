6b500454fea710d66ede4ffeb98ade5310539a23
Htein Lin Aung <hteinlinaung1423@gmail.com>
2020-11-13 00:40:44 +0800

    make it fullscreen

--- dist/js/home.js
+++ dist/js/home.js
@@ -20,6 +20,11 @@ let app = new Application({
 }
 );
 
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
