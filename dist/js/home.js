9cf7dce68e66e8c7e26d61611e8c1f68a3de0004
Htein Lin Aung <hteinlinaung1423@gmail.com>
2020-11-13 00:23:16 +0800

    test pixi

--- dist/js/home.js
+++ dist/js/home.js
@@ -1,5 +1,17 @@
 //Create a Pixi Application
-let app = new PIXI.Application({ width: 256, height: 256 });
+let app = new PIXI.Application({
+    width: 256,         // default: 800
+    height: 256,        // default: 600
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1       // default: 1
+}
+);
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
